bc06f9ad5791868b97002577e6165d25c1fa0286
Wesley <wesley@aquabyte.gg>
2024-09-26 23:28:04 -0400

    constant issues with postgres SMH.... Will make progress tmr.

--- storm-stream/services/chat-service/build.gradle.kts
+++ storm-stream/services/chat-service/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("org.springframework.boot") version "3.3.4"
+}
+
 dependencies {
     implementation("org.springframework.kafka:spring-kafka:3.2.4")
     implementation("org.springframework.cloud:spring-cloud-stream:4.1.3")
@@ -6,4 +10,20 @@ dependencies {
     implementation("org.springframework.data:spring-data-jpa:3.3.4")
     implementation("org.springframework.boot:spring-boot-starter-web:3.3.4")
     implementation(project(":commons"))
+  //  implementation("org.flywaydb:flyway-core:10.0.0")
+    runtimeOnly("org.postgresql:postgresql:9.3-1101-jdbc41")
+}
+
+springBoot {
+    mainClass.set("com.wtench.ChatPersistenceApplication")
+}
+
+application {
+    mainClass.set("com.wtench.ChatPersistenceApplication")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.wtench.ChatPersistenceApplication"
+    }
 }
--- storm-stream/services/chat-service/src/main/java/com/wtench/ChatPersistenceApplication.java
+++ storm-stream/services/chat-service/src/main/java/com/wtench/ChatPersistenceApplication.java
@@ -1,15 +1,18 @@
 package com.wtench;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 
+@Configuration
 @SpringBootApplication
 @EnableKafka
 public class ChatPersistenceApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(ChatPersistenceApplication.class, args);
     }
 }
